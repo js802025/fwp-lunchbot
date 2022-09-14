a46680ffd3fa6a9cf5bd213a0fcfc618c2c50d96
js802025 <30957566+js802025@users.noreply.github.com>
2022-09-13 19:36:38 -0500

    revert

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def sms():
     # number = request.form['From'] # variable not needed
     # message_body = request.form['Body'].title().strip() # Who cares
     resp = MessagingResponse()
-    response = bot.get_week()#+"\nSigning in to the portal is not that easy, but Skilldule can help. On Skilldule you can access your schedule, homework, lunch, MXes and even group chats for your specific classes all in the same time it took to text this bot. Set up your Skilldule today at: https://skilldule.herokuapp.com"
+    response = bot.get_week()+"\nSigning in to the portal is not that easy, but Skilldule can help. On Skilldule you can access your schedule, homework, lunch, MXes and even group chats for your specific classes all in the same time it took to text this bot. Set up your Skilldule today at: https://skilldule.herokuapp.com"
 
     resp.message(response)
 
--- lunch_menu_parser.py
+++ lunch_menu_parser.py
@@ -45,7 +45,7 @@ class Lunchbot:
         week_menu = ""
         for day, lunch in self.menu.items():
             week_menu += day+":\n"+lunch+"\n\n"
-        return week_menu.replace("(", "").replace(")", "").replace("/", "").replace(":", "")
+        return week_menu#.replace("(", "").replace(")", "").replace("/", "").replace(":", "")
 
 
     def get_day(self, day):
@@ -64,5 +64,4 @@ if __name__ == '__main__':
     bot = Lunchbot(url)
 
     todays_menu = bot.get_week()
-
     print(todays_menu)
